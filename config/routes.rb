2400829a4b7c4cb4feb2c053b63b374bad4631bb
Jaya0112 <jayakrishnaveni19@gmail.com>
2022-03-20 16:46:27 +0000

    employee management

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  root 'employees#index'
+
+  resources :employees
 end
---
+++ db/migrate/20220320163111_add_employee.rb
@@ -0,0 +1,12 @@
+class AddEmployee < ActiveRecord::Migration[6.1]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.integer :age
+      t.string :gender
+      t.string :designation
+
+      t.timestamps
+    end
+  end
+end
